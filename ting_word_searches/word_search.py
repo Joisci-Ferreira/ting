f6b7583e39aadc037aae71ff8ecf55e5331e3f90
Joiscilene <joisci.lene@hotmail.com>
2022-12-09 20:50:21 -0300

    Adicionando função exists word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,24 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    result = list()
+    occurrences = list()
+    for index in range(len(instance)):
+        search = instance.search(index)
+        lines = search['linhas_do_arquivo']
+        for index, line in enumerate(lines):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index + 1})
 
+        if len(occurrences) >= 1:
+            search_result = {
+                "palavra": word,
+                "arquivo": search["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            }
+            result.append(search_result)
+
+    return result  
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    
